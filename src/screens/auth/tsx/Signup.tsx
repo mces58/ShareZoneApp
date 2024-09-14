ab5b2c0c09dcbde15771effd8e5e5a918d55ce88
mces58 <ceser446@gmail.com>
2024-09-15 02:06:59 +0300

    fix(ui): update Header component to use useSafeAreaInsets for status bar height

--- src/screens/auth/tsx/Signup.tsx
+++ src/screens/auth/tsx/Signup.tsx
@@ -7,7 +7,7 @@ import { createSignupFormFields } from './feats/signup-form';
 import SpacemanWithMoonSvg from 'assets/svgs/spaceman-with-moon.svg';
 import Icon from 'src/assets/icons';
 import { GradientButton } from 'src/components/buttons';
-import { BaseContainer, Container } from 'src/components/containers';
+import { Container } from 'src/components/containers';
 import BaseForm from 'src/components/forms/Base';
 import BaseHeader from 'src/components/headers/Base';
 import { BaseText, GradientText } from 'src/components/texts';
@@ -78,121 +78,112 @@ const Signup: React.FC<SignupProps> = ({ navigation }) => {
   }, []);
 
   return (
-    <BaseContainer>
-      <Container flexStyle={styles.flex.container}>
-        <BaseHeader
-          title={t('global.back')}
-          icon={
-            <Icon
-              name="short-arrow"
-              direction="left"
-              onPress={() => navigation.goBack()}
+    <Container flexStyle={styles.flex.container}>
+      <BaseHeader
+        title={t('global.back')}
+        icon={
+          <Icon name="short-arrow" direction="left" onPress={() => navigation.goBack()} />
+        }
+        flexStyle={styles.flex.header}
+        shadowStyle={styles.shadow.header}
+        textStyle={styles.text.header}
+        viewStyle={styles.view.header}
+      />
+      <Container flexStyle={styles.flex.main}>
+        <Container flexStyle={styles.flex.form}>
+          <GradientText
+            text={t('auth.signUp')}
+            colors={theme.common.color.defaultGradient2}
+            textStyle={styles.text.formHeader}
+          />
+          <BaseForm
+            formFields={formFields}
+            onSubmit={handleSignup}
+            ref={formRef}
+            inputStyle={{
+              flex: styles.flex.formInput,
+              shadow: styles.shadow.formInput,
+              view: styles.view.formInput,
+            }}
+          />
+          {toast && (
+            <Toast
+              message={toast.message}
+              type={toast.type}
+              icon={
+                toast.type === ToastType.Error ? (
+                  <Icon name="error" fillColor={theme.color.text} strokeWidth={0} />
+                ) : (
+                  <Icon name="check" fillColor={theme.color.text} strokeWidth={0} />
+                )
+              }
             />
-          }
-          flexStyle={styles.flex.header}
-          shadowStyle={styles.shadow.header}
-          textStyle={styles.text.header}
-          viewStyle={styles.view.header}
-        />
-        <Container flexStyle={styles.flex.main}>
-          <Container flexStyle={styles.flex.form}>
-            <GradientText
-              text={t('auth.signUp')}
-              colors={theme.common.color.defaultGradient2}
-              textStyle={styles.text.formHeader}
+          )}
+        </Container>
+        <Container flexStyle={styles.flex.footer}>
+          <Container flexStyle={styles.flex.footerImage}>
+            <SpacemanWithMoonSvg
+              width={scaleByAspectRatio(200)}
+              height={scaleByAspectRatio(200)}
             />
-            <BaseForm
-              formFields={formFields}
-              onSubmit={handleSignup}
-              ref={formRef}
-              inputStyle={{
-                flex: styles.flex.formInput,
-                shadow: styles.shadow.formInput,
-                view: styles.view.formInput,
-              }}
-            />
-            {toast && (
-              <Toast
-                message={toast.message}
-                type={toast.type}
-                icon={
-                  toast.type === ToastType.Error ? (
-                    <Icon name="error" fillColor={theme.color.text} strokeWidth={0} />
-                  ) : (
-                    <Icon name="check" fillColor={theme.color.text} strokeWidth={0} />
-                  )
-                }
-              />
-            )}
           </Container>
-          <Container flexStyle={styles.flex.footer}>
-            <Container flexStyle={styles.flex.footerImage}>
-              <SpacemanWithMoonSvg
-                width={scaleByAspectRatio(200)}
-                height={scaleByAspectRatio(200)}
+          <Container flexStyle={styles.flex.footerAction}>
+            <Container flexStyle={styles.flex.footerButtonContainer}>
+              <GradientButton
+                text={t('auth.signUp')}
+                colors={theme.common.color.defaultGradient2}
+                onPress={handleFormSubmit}
+                loading={loading}
+                flexStyle={styles.flex.button}
+                shadowStyle={styles.shadow.button}
+                textStyle={styles.text.button}
+                viewStyle={styles.view.button}
               />
-            </Container>
-            <Container flexStyle={styles.flex.footerAction}>
-              <Container flexStyle={styles.flex.footerButtonContainer}>
-                <GradientButton
-                  text={t('auth.signUp')}
-                  colors={theme.common.color.defaultGradient2}
-                  onPress={handleFormSubmit}
-                  loading={loading}
-                  flexStyle={styles.flex.button}
-                  shadowStyle={styles.shadow.button}
-                  textStyle={styles.text.button}
-                  viewStyle={styles.view.button}
-                />
-                <Container flexStyle={styles.flex.rowText}>
-                  <BaseText
-                    text={t('auth.alreadyHaveAnAccount')}
-                    textStyle={styles.text.footer}
-                  />
-                  <GradientText
-                    text={t('auth.signIn')}
-                    colors={theme.common.color.defaultGradient2}
-                    onPress={() => {
-                      if (!formRef.current) return;
-                      navigation.navigate(NavigationRoutes.SIGNIN);
-                      formRef.current.reset();
-                    }}
-                    textStyle={styles.text.link}
-                  />
-                </Container>
-              </Container>
-              <Container flexStyle={styles.flex.follow}>
+              <Container flexStyle={styles.flex.rowText}>
                 <BaseText
-                  text={t('auth.followOn') + ':'}
-                  textStyle={styles.text.follow}
+                  text={t('auth.alreadyHaveAnAccount')}
+                  textStyle={styles.text.footer}
                 />
-                <Icon
-                  name="github"
-                  size={scaleByAspectRatio(18)}
-                  color={{ isGradient: true, grads: theme.common.color.defaultGradient1 }}
-                  onPress={() => Linking.openURL('https://github.com/mces58')}
-                />
-                <Icon
-                  name="instagram"
-                  size={scaleByAspectRatio(18)}
-                  color={{
-                    isGradient: true,
-                    grads: theme.common.color.defaultGradient1,
+                <GradientText
+                  text={t('auth.signIn')}
+                  colors={theme.common.color.defaultGradient2}
+                  onPress={() => {
+                    if (!formRef.current) return;
+                    navigation.navigate(NavigationRoutes.SIGNIN);
+                    formRef.current.reset();
                   }}
-                  onPress={() => Linking.openURL('https://www.instagram.com/mces58')}
-                />
-                <Icon
-                  name="linkedin"
-                  size={scaleByAspectRatio(18)}
-                  color={{ isGradient: true, grads: theme.common.color.defaultGradient1 }}
-                  onPress={() => Linking.openURL('https://www.linkedin.com/in/mces58')}
+                  textStyle={styles.text.link}
                 />
               </Container>
+            </Container>
+            <Container flexStyle={styles.flex.follow}>
+              <BaseText text={t('auth.followOn') + ':'} textStyle={styles.text.follow} />
+              <Icon
+                name="github"
+                size={scaleByAspectRatio(18)}
+                color={{ isGradient: true, grads: theme.common.color.defaultGradient1 }}
+                onPress={() => Linking.openURL('https://github.com/mces58')}
+              />
+              <Icon
+                name="instagram"
+                size={scaleByAspectRatio(18)}
+                color={{
+                  isGradient: true,
+                  grads: theme.common.color.defaultGradient1,
+                }}
+                onPress={() => Linking.openURL('https://www.instagram.com/mces58')}
+              />
+              <Icon
+                name="linkedin"
+                size={scaleByAspectRatio(18)}
+                color={{ isGradient: true, grads: theme.common.color.defaultGradient1 }}
+                onPress={() => Linking.openURL('https://www.linkedin.com/in/mces58')}
+              />
             </Container>
           </Container>
         </Container>
       </Container>
-    </BaseContainer>
+    </Container>
   );
 };
 
